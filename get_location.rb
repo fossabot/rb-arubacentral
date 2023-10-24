2a9a2b42b8ab551fe6aeaaaf1dc413526ae7dbc0
nilsver <139124949+nilsver@users.noreply.github.com>
2023-10-24 15:30:26 +0100

    Add files via upload
    Cleaner version of get_all_access_points_in_campus and removed campus_id parameter

--- get_location.rb
+++ get_location.rb
@@ -27,36 +27,12 @@ def get_data(api_token, api_endpoint)
   
   if response.code.to_i == 200
     data = JSON.parse(response.body)
-    return data.to_json
+    return data
   else
     puts "api request failed with status code: #{response.code}"
     puts "Response.body: #{response.body}"
     return nil
   end
-end
-
-def get_all_campuses(api_token)
-  get_data(api_token, 'visualrf_api/v1/campus')
-end
-
-def get_campus(api_token, campus_id)
-  get_data(api_token, "visualrf_api/v1/campus/#{campus_id}")
-end
-
-def get_building(api_token, building_id)
-  get_data(api_token, "visualrf_api/v1/building/#{building_id}")
-end
-
-def get_floor(api_token, floor_id)
-  get_data(api_token, "visualrf_api/v1/floor/#{floor_id}")
-end
-
-def get_access_points(api_token, floor_id)
-  get_data(api_token, "visualrf_api/v1/floor/#{floor_id}/access_point_location")
-end
-
-def get_access_point_location(api_token, ap_id)
-  get_data(api_token, "visualrf_api/v1/access_point_location/#{ap_id}")
 end
 
 #topic is not created yet
@@ -70,44 +46,37 @@ def produce_to_kafka(msg, topic='rb_arubacentral')
   end
 end
 
-#if we there is just 1 fixed floor, we can use this.
-#def get_location_ap_in_floor(api_token, floor_id)
-#  access_points = get_access_points(api_token, floor_id)
-#  access_points['access_points'].each do |ap|
-#    ap_id = access_point['ap_id']
-#    access_point_location = JSON.parse(get_access_point_location(api_token, ap_id)
-#    produce_to_kafka(access_point_location, @producer)
-#  end
-#end
-
 #if there might be floors/buildings added in the future
-def get_all_access_points_in_campus(api_token, campus_id)
-  campus_data = JSON.parse(get_campus(api_token, campus_id))
-  buildings = campus_data['buildings']
-  all_data = []
+def get_all_access_points_in_campus(api_token)
+  campuses = get_data(api_token, "visualrf_api/v1/campus")
+ 
+  campuses['campus'].each do |campus|
+    campus_id = campus['campus_id']
+    campus_data = get_data(api_token, "visualrf_api/v1/campus/#{campus_id}")
+    #buildings = campus_data['buildings']
 
-  buildings.each do |building|
-    building_id = building['building_id']
-    floors = JSON.parse(get_building(api_token, building_id))['floors']
-    if floors.nil?
-      next
-    end
+    #buildings.each do |building|
+    campus_data['buildings'].each do |building|
+      building_id = building['building_id']
+      building_data = get_data(api_token, "visualrf_api/v1/building/#{building_id}")
+      #floors = building_data['floors']
 
-    floors.each do |floor|
-      floor_id = floor['floor_id']
-      access_points = JSON.parse(get_access_points(api_token, floor_id))
-      access_points['access_points'].each do |access_point|
-        ap_id = access_point['ap_id']
-        access_point_location = JSON.parse(get_access_point_location(api_token, ap_id))
-        puts access_point_location
+      #floors.each do |floor|
+      building_data['floors'].each do |floor|
+        floor_id = floor['floor_id']
+        access_points = get_data(api_token, "visualrf_api/v1/floor/#{floor_id}/access_point_location")
+     
+        access_points['access_points'].each do |access_point|
+          ap_id = access_point['ap_id']
+          access_point_location = get_data(api_token, "visualrf_api/v1/access_point_location/#{ap_id}")
+          puts access_point_location
+        end
       end
     end
   end
-  return all_data.to_json
 end 
 
-api_token = 'cmu0zwI2Mpd0DQkZpAeXH9SSbAobbcUD'
+api_token = 'rJYnlw6FcGID6x6b0WGfBzrveMQe1AnI'
 
-get_all_access_points_in_campus(api_token, '2797a84e69ab11ed977ed6dd37c16971__default')
-
+get_all_access_points_in_campus(api_token)
 
